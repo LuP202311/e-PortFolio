9d7a40c2018a12ed2c1d527a4e756e513237c430
RefonteSite <lucie.phung@etu.u-paris.fr>
2025-03-23 00:28:12 +0100

    modification du projet - création d'un serveur en node.js

---
+++ serveur/serveur.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const path = require("path");
+
+// Serve les fichiers statiques (HTML, CSS, JS)
+app.use(express.static(path.join(__dirname, '../app')));
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../app', 'index.html'));
+});
+
+// Écouter sur le port 6500
+const PORT = 6500;
+app.listen(PORT, () => {
+    console.log(`Serveur démarré sur http://localhost:${PORT}`);
+});
